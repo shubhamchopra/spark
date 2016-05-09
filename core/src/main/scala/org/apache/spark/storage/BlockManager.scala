86e1e0212b0dae0d598f0128c6a7b8f33429dc27
Shubham Chopra <schopra31@bloomberg.net>
2016-05-09 16:58:21 -0400

    Adding null check so a Block Manager can be initiaziled without the master.

--- core/src/main/scala/org/apache/spark/storage/BlockManager.scala
+++ core/src/main/scala/org/apache/spark/storage/BlockManager.scala
@@ -164,7 +164,7 @@ private[spark] class BlockManager(
 
     val rackInfo = {
       val rackStr = master.getRackInfo(blockTransferService.hostName)
-      if (rackStr.isEmpty) {
+      if (rackStr == null || rackStr.isEmpty) {
         None
       } else {
         Some(rackStr)
